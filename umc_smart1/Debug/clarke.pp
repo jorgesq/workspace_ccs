3fec73026f51321cf4dd3b6b2a5c4739d1dfe3e7
Michal Elias <eldis.cz@gmail.com>
2016-07-11 08:45:00 +0200

    Smart EV project in the workspace

---
+++ umc_smart1/Debug/clarke.pp
@@ -0,0 +1,29 @@
+# FIXED
+
+clarke.obj: C:/work/git/umclib/modules/clarke/src/32b/clarke.c
+clarke.obj: C:/work/git/umclib/modules/clarke/src/32b/clarke.h
+clarke.obj: C:/work/git/umclib/modules/iqmath/src/32b/IQmathLib.h
+clarke.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.10/include/limits.h
+clarke.obj: C:/work/git/umclib/modules/math/src/32b/math.h
+clarke.obj: C:/work/git/umclib/modules/types/src/types.h
+clarke.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.10/include/stdbool.h
+clarke.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.10/include/yvals.h
+clarke.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.10/include/stdarg.h
+clarke.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.10/include/linkage.h
+clarke.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.10/include/_lock.h
+clarke.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.10/include/string.h
+clarke.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.10/include/stdint.h
+
+C:/work/git/umclib/modules/clarke/src/32b/clarke.c: 
+C:/work/git/umclib/modules/clarke/src/32b/clarke.h: 
+C:/work/git/umclib/modules/iqmath/src/32b/IQmathLib.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.10/include/limits.h: 
+C:/work/git/umclib/modules/math/src/32b/math.h: 
+C:/work/git/umclib/modules/types/src/types.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.10/include/stdbool.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.10/include/yvals.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.10/include/stdarg.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.10/include/linkage.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.10/include/_lock.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.10/include/string.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.10/include/stdint.h: 
